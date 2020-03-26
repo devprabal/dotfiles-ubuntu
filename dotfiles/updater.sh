6fb9ff23356da80ac6e1d0e9b1cb9fa6c9d335c8
devprabal <ghjxvnjd@gmail.com>
2020-03-26 19:32:08 +0530

    Compilation of works

---
+++ dotfiles/updater.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt update; sudo apt upgrade -y; sudo apt install -f; sudo apt autoremove -y; sudo apt autoclean -y; sudo apt install -f; sudo snap refresh;
---
+++ updater.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt update; sudo apt upgrade -y; sudo apt install -f; sudo apt autoremove -y; sudo apt autoclean -y; sudo apt install -f; sudo snap refresh;
+sudo flatpak update;
